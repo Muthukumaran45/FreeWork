716fc512e5c5b7932b588dc8e45689cf9247fc0d
Muthu kumaran <129528407+Muthukumaran45@users.noreply.github.com>
2024-05-05 06:30:36 +0530

    Update Message.jsx

--- Src/Components/Pages/Message.jsx
+++ Src/Components/Pages/Message.jsx
@@ -7,18 +7,11 @@ import CardRoom from '../Chat/CardRoom';
 import ChatRoom from '../Chat/ChatRoom';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLK3Wo4ltCL5bBo2ZmGqiqs78rRwyuSKo",
-  authDomain: "sample-a2b07.firebaseapp.com",
-  databaseURL: "https://sample-a2b07-default-rtdb.firebaseio.com",
-  projectId: "sample-a2b07",
-  storageBucket: "sample-a2b07.appspot.com",
-  messagingSenderId: "187602197493",
-  appId: "1:187602197493:web:e61ce7ba3b50c25a27debc"
+// paste your firebaseConfig here
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+// you need to initialize here
 
 function createChatRoom(participantId, username) {
   const chatRoomId = [participantId, username].sort().join('-');
